d5e68182daf1350c25b36c129324ac8be29ed9c8
Didier Delhaye <didier.delhaye@drkdidel.be>
2019-12-20 13:22:25 +0100

    Initial Commit

---
+++ swift-exercice10/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-exercice10
+//
+//  Created by Vagrant on 20/12/2019.
+//  Copyright © 2019 Technifutur. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
