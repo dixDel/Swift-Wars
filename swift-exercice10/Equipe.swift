7eedb64b83f46436ad2d3c3f49bb58ca5f1a2551
Didier Delhaye <didier.delhaye@drkdidel.be>
2019-12-23 00:00:46 +0100

    5 vs 5 fights

--- swift-exercice10/Equipe.swift
+++ swift-exercice10/Equipe.swift
@@ -39,50 +39,102 @@ class Equipe {
     // @TODO event listener qui check résultat de l’attaque
     // event listener sur Assassin: quand attaqué, tente une esquive
     func attaque(ennemi: Equipe) {
-        if let perso1 = getFighter() {
-            if let opponent1 = ennemi.getFighter() {
-                if perso1 is Magicien {
-                    let mago: Magicien = perso1 as! Magicien
+        var team = getFightingTeam()
+        var foes = ennemi.getFightingTeam()
+        var isFoeFighting = true
+        var cpt = 0
+        
+        while isFoeFighting && cpt < team.count {
+            let fighter = team[cpt]
+            // ordre d’attaque:
+            // - Pal, Mag: Pal, Ass, Mag
+            // - Ass: Mag, Pal, Ass
+            if fighter is Paladin || fighter is Magicien {
+                foes.sort { (perso1, perso2) -> Bool in
+                    var isFirst = true
+                    if perso1 is Assassin && perso2 is Paladin || perso1 is Magicien {
+                            isFirst = false
+                    }
+                    return isFirst
+                }
+            } else {
+                foes.sort { (perso1, perso2) -> Bool in
+                    var isFirst = true
+                    if perso1 is Paladin && perso2 is Magicien || perso1 is Assassin {
+                        isFirst = false
+                    }
+                    return isFirst
+                }
+            }
+            
+            if let foe: Personnage = getOpponent(foes: foes) {
+                if fighter is Magicien {
+                    let mago: Magicien = fighter as! Magicien
                     if let hurtFighter = persos.first(where: { $0.name != mago.name && $0.isHurt() }) { // ne peut pas se soigner lui-même
                         mago.hurtFighter = hurtFighter
                     }
                 }
                 var ass: Assassin?
-                if opponent1 is Assassin {
-                    ass = opponent1 as? Assassin
+                if foe is Assassin {
+                    ass = foe as? Assassin
                 }
-                if (ass == nil || !(ass?.evadeAttack(ennemi: perso1) ?? false))  {
-                    perso1.attaque(ennemi: opponent1)
+                if (ass == nil || !(ass?.evadeAttack(ennemi: fighter) ?? false)) {
+                    fighter.attaque(ennemi: foe)
                 }
+            } else {
+                print("Plus d’ennemis en état de combattre !")
+                isFoeFighting = false
             }
+            cpt += 1
         }
+        
+        dismissFightingTeam(team: &team)
+        ennemi.dismissFightingTeam(team: &foes)
     }
     
     func getFightingTeam() -> [Personnage] {
         var team: [Personnage] = [Personnage]()
-        for _ in 1...fightingTeamSize {
+        var hasFighters = true
+        var cpt = 0
+        print("Équipe de \(name):")
+        while hasFighters && cpt < fightingTeamSize {
             if let fighter = getFighter() {
                 team.append(fighter)
+                print("\(fighter.name)")
             } else {
-                print("L’équipe \(name) n’a pas assez de combattants: \(persos.count) !")
+                hasFighters = false
+                print("Plus de combattants !")
             }
+            cpt += 1
         }
         return team
     }
     
     func getFighter() -> Personnage? {
-        return persos.remove(at: Int.random(in: 0...persos.count))
-    }
-    
-    func dismissFightingTeam(team: [Personnage]) {
-        persos.append(contentsOf: team)
-    }
-    
-    func buryCorpses() {
-        if let index = persos.firstIndex(where: { $0.isKilled() }) {
-            print("\(persos[index].name) a rendu les armes...")
-            persos.remove(at: index)
+        var fighter: Personnage? = nil
+        if persos.count > 0 {
+            fighter = persos.remove(at: Int.random(in: 0..<persos.count))
         }
+        return fighter
+    }
+    
+    // foes ont été triés selon la classe du fighter dans la méthode attaque
+    func getOpponent(foes: [Personnage]) -> Personnage? {
+        return foes.first(where: {
+            !$0.isKilled()
+        })
+    }
+    
+    func dismissFightingTeam(team: inout [Personnage]) {
+        team.removeAll { (fighter) -> Bool in
+            var isKilled = false
+            if fighter.isKilled() {
+                isKilled = true
+                print("\(fighter.name) a rendu les armes...")
+            }
+            return isKilled
+        }
+        persos += team
     }
     
     func isStillFighting() -> Bool {
--- swift-exercice10/main.swift
+++ swift-exercice10/main.swift
@@ -19,13 +19,17 @@ let lesMauvais = Equipe(name: "les mauvais")
 // chaque tour jusqu’à ce qu’une équipe n’ait plus de perso:
 // 5 persos vs 5 persos (sélection aléatoire)
 var isMatchEnded = false
+var cptRound = 1
 while !isMatchEnded {
     //@TODO fair-play: randomly choose first attacker
+    print("Round \(cptRound)")
     lesBons.attaque(ennemi: lesMauvais)
-    lesMauvais.attaque(ennemi: lesBons)
-    lesBons.buryCorpses()
-    lesMauvais.buryCorpses()
+    if lesMauvais.isStillFighting() {
+        lesMauvais.attaque(ennemi: lesBons)
+    }
     isMatchEnded = !lesBons.isStillFighting() || !lesMauvais.isStillFighting()
+    cptRound += 1
+    print()
 }
 print("Guerre terminée !")
 print("Survivants: \(lesBons.nbSurvivants()) bons, \(lesMauvais.nbSurvivants()) mauvais.")
