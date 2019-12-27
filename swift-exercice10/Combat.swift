715a4fd74bcef437d3af5a3b6d5c3c278d61082e
Didier Delhaye <didier.delhaye@drkdidel.be>
2019-12-27 17:17:31 +0100

    choose the first attacker randomly

--- swift-exercice10/Combat.swift
+++ swift-exercice10/Combat.swift
@@ -10,13 +10,17 @@ class Combat {
     let lesBons: Equipe
     let lesMauvais: Equipe
     
-    var cptRound: Int = 1
+    var cptRound: Int = 0
+    var firstAttacker: Equipe
+    var secondAttacker: Equipe
     
     init() {
         // init équipes: 100 persos/équipe
         // > 10 à 20 Ass, 15 à 25 Mag, reste Pal
         lesBons = Equipe(name: "les bons")
         lesMauvais = Equipe(name: "les mauvais")
+        firstAttacker = lesBons
+        secondAttacker = lesMauvais
         print(lesBons)
         print(lesMauvais)
         print()
@@ -24,22 +28,30 @@ class Combat {
     
     // 5 persos vs 5 persos (sélection aléatoire)
     func playRound() -> Bool {
-        // init Equipes
-        // lance attaques
-
-        //@TODO fair-play: randomly choose first attacker
+        cptRound += 1
         //utiliser les mêmes équipes dans un round (défenseurs attaquent ensuite)
         print("Round \(cptRound)")
-        lesBons.attaque(ennemi: lesMauvais)
-        if lesMauvais.isStillFighting() {
-            lesMauvais.attaque(ennemi: lesBons)
+        drawTurns()
+        firstAttacker.attaque(ennemi: secondAttacker)
+        if secondAttacker.isStillFighting() {
+            secondAttacker.attaque(ennemi: firstAttacker)
         }
-        cptRound += 1
         print()
-        return !lesBons.isStillFighting() || !lesMauvais.isStillFighting()
+        return !firstAttacker.isStillFighting() || !secondAttacker.isStillFighting()
     }
     
     func showResults() {
-        print("Survivants: \(lesBons.nbSurvivants()) bons, \(lesMauvais.nbSurvivants()) mauvais.")
+        print("Survivants après \(cptRound) rounds : \(lesBons.nbSurvivants()) bons, \(lesMauvais.nbSurvivants()) mauvais.")
+    }
+    
+    func drawTurns() {
+        switch Int.random(in: 0...1) {
+        case 1:
+            firstAttacker = lesMauvais
+            secondAttacker = lesBons
+        default:
+            firstAttacker = lesBons
+            secondAttacker = lesMauvais
+        }
     }
 }
